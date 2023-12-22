8091de47ccd7571cdbcdfd09a0ea5e0162b05561
shivam datta purkayastha <shivamdatta465@gmail.com>
2023-12-22 20:47:59 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pyttsx3
+if __name__=="__main__":
+    text_to_speech=pyttsx3.init()
+    while True:
+        word=input("Enter your command")
+        if word=="q":
+            break
+        text_to_speech.say(word)
+        text_to_speech.runAndWait()
